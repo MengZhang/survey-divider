18c23c7f6c26e16a6d6a612967d68da59dee9a32
Christopher Villalobos <cvillalobos@ufl.edu>
2016-05-23 00:35:03 -0400

    Initial setup for library-based data partitioning.
    * Works for ACEB files
    * Can also split by size and mix-and-match

--- src/main/java/org/agmip/tools/surveydivider/Main.java
+++ src/main/java/org/agmip/tools/surveydivider/Main.java
@@ -1,8 +1,16 @@
 package org.agmip.tools.surveydivider;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import org.agmip.ace.AceDataset;
+import org.agmip.ace.AceWeather;
+import org.agmip.ace.io.AceGenerator;
+import org.agmip.ace.io.AceParser;
+import org.agmip.cropmodel.data.partition.PartitionBuilder;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -10,59 +18,53 @@ import org.slf4j.LoggerFactory;
  * @author Meng Zhang
  */
 public class Main {
-    
-    
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(Main.class);
-    private static File surveyFile;
-    private static File linkageFile;
-    private static File wthData;
-    private static File culData;
-    private static File soilData;
-    private static File overlyDomeFile;
-    private static File strategyDomeFile;
-//    private static File outputDir = new File("output");
-    
-    private static File workDir;
-    private static HashMap<String, File> wthFiles;
-    private static HashMap<String, File> soilFiles;
-    
+
     public static void main(String[] args) throws Exception {
-        
-        surveyFile = new File(args[0]);
-        soilData = new File(args[1]);
-        wthData = new File(args[2]);
-        culData = new File(args[3]);
-        linkageFile = new File(args[4]);
-        overlyDomeFile = new File(args[5]);
-        strategyDomeFile = new File(args[6]);
-        workDir = new File(args[7]);
-        scanData();
-        divideByWth();
+        File acebFile = new File(args[0]);
+        String workingPath;
+        if (args.length < 1) {
+            workingPath = ".";
+        } else{
+            workingPath = args[1];
+        }
+
+        AceDataset dataset = AceParser.parseACEB(acebFile);
+        PartitionBuilder p = new PartitionBuilder(dataset);//.byWeatherStation();
+        p.bySize(8);
+        p.byWeatherStation();
+        System.out.println("Final partition count: " + p.size());
+        for(Map.Entry<String, AceDataset> entry : p.get().entrySet()) {
+            Path outputDir = Paths.get(workingPath + File.separator + entry.getKey());
+            Files.createDirectories(outputDir);
+            AceGenerator.generateACEB(outputDir.resolve("SurveyData.aceb").toFile(), entry.getValue());
+        }
     }
-    
+    /*
     public static void scanData() throws IOException {
         wthFiles = SDUtil.scanWthData(wthData, workDir);
         soilFiles = SDUtil.scanSoilData(soilData, workDir);
-        
     }
-    
+
     private static void divideByWth() throws IOException {
-        
         File divDir = SDUtil.mkdir(workDir, "divided", true, true);
-        
+
         // Deploy weather files
         for (String wthID : wthFiles.keySet()) {
             String wstID = SDUtil.getWstID(wthID);
             File wthDir = SDUtil.mkdir(divDir, wstID, true, false);
             SDUtil.deployFile(wthDir, wthFiles.get(wthID));
+
         }
-        
+
         // SCAN exp file, devide it and deploy the small pieces of exp file
         // deploy the soil file based on the soil_id list
         SDUtil.divideExpByWth(surveyFile, soilFiles, divDir);
         SDUtil.deployCulFileByWth(culData, divDir);
-        
+
         // make input package for QuadUI
         SDUtil.createBatchFilebyWth(divDir, overlyDomeFile, strategyDomeFile, linkageFile);
     }
+    */
 }
+
